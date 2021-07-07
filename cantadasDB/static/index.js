6a6714b5b07807c6dc95acca2271b376f3519b0c
João <hlabeats@gmail.com>
2021-07-07 18:23:48 -0300

    Update index.js

--- cantadasDB/static/index.js
+++ cantadasDB/static/index.js
@@ -1,18 +1,7 @@
 function iniciar(){
   document.getElementById("copy").addEventListener("click",copiar_texto);
-  document.getElementById("copyRow").addEventListener("click", copiar_linha);
 }
 function copiar_texto() {
     navigator.clipboard.writeText(document.getElementById("chamada").textContent+" "+document.getElementById("paquera").textContent);
     alert("Copiada a cantada: "+ document.getElementById("chamada").textContent+" "+document.getElementById("paquera").textContent);
-}
-
-function copiar_linha() {
-  let tmpElement = $('<textarea style="opacity:0;"></textarea>');
-  let parent = $(this).closest('td').siblings().each(function(){
-    tmpElement.text(tmpElement.text() + $(this).text() + '\t');
-  }); 
-  tmpElement.appendTo($('body')).focus().select();
-  document.execCommand("copy");
-  tmpElement.remove();
 }
