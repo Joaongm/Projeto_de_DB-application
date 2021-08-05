127d1a48483b4ffe0be803eaf0ce3224f641dcc8
João <83662444+Joaongm@users.noreply.github.com>
2021-08-05 01:36:20 -0300

    Update scriptinicial.sql

--- scriptinicial.sql
+++ scriptinicial.sql
@@ -4,9 +4,3 @@ chamada varchar(255)not null,
 paquera varchar(255) not null
 )
 
-insert into cantadas(chamada, paquera) values('oi, meu mel', 'vc já viu aquele filme? o místerio de benjaminha boca');
-insert into cantadas(chamada, paquera) values('Dr. Sonic confirma!', 'Seu diagnóstico deu positivo para GOSTOSA');
-insert into cantadas(chamada, paquera) values('ei meu anjo','se meu coração fosse uma favela, eu te dava um barraco');
-insert into cantadas(chamada, paquera) values('que carinha mais linda vc tem','será que posso sentar nela?');
-
-select * from public.cantadas 
